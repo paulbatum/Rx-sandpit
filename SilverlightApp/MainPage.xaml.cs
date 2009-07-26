6b67ca8ddd34b58161e014e552e7b22a01c489d0
Paul Batum <paul.batum@gmail.com>
2009-07-26 13:22:38 +1000

    Initial commit for rx sandpit

---
+++ SilverlightApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightApp
+{
+    public partial class MainPage : UserControl
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+            IObservable<Event<RoutedEventArgs>> clicks = Observable.FromEvent<RoutedEventArgs>(button1, "Click");
+
+            int count = 0;
+            clicks.Subscribe(() => count++);
+
+            IObservable<string> messages = from c in clicks
+                                           select string.Format("Clicked {0} time{1}", count, count > 1 ? "s" : "");
+            messages.Subscribe(s => button1.Content = s);
+
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IObservable<Event<RoutedEventArgs>> GetClicks(this Button button)
+        {
+            return Observable.FromEvent((EventHandler<RoutedEventArgs> genericHandler) => new RoutedEventHandler(genericHandler),
+                                        routedHandler => button.Click += routedHandler,
+                                        routedHandler => button.Click -= routedHandler);
+        }
+
+    }
+
+}
