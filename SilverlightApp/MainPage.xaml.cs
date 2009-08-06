8338160d254d4f8d612cd889f01ec6c3c46e54e6
Paul Batum <paul.batum@gmail.com>
2009-08-06 23:41:12 +1000

    Combination example, with the ability to select different mechanisms

--- SilverlightApp/MainPage.xaml.cs
+++ SilverlightApp/MainPage.xaml.cs
@@ -20,25 +20,42 @@ namespace SilverlightApp
         {
             InitializeComponent();
 
-            string formatString = "Option {0}, Option {1}, Option {2}";
+            var observerSelections = Observable.Merge(
+                Observable.FromEvent<RoutedEventArgs>(rbSelectMany, "Checked")
+                    .Select(_ => from s1 in choiceControl1.OptionSelections
+                                 from s2 in choiceControl2.OptionSelections
+                                 from s3 in choiceControl3.OptionSelections
+                                 select new[] { s1, s2, s3 }),
+                Observable.FromEvent<RoutedEventArgs>(rbForkJoin, "Checked")
+                    .Select(_ => Observable.ForkJoin(choiceControl1.OptionSelections, choiceControl2.OptionSelections, choiceControl3.OptionSelections)),
+                Observable.FromEvent<RoutedEventArgs>(rbCombineLatest, "Checked")
+                    .Select(_ => choiceControl1.OptionSelections
+                                    .CombineLatest(choiceControl2.OptionSelections, (i, j) => new[] { i, j })
+                                    .CombineLatest(choiceControl3.OptionSelections, (array, k) => new[] { array[0], array[1], k }))
+                );
 
-            //var selections = from s1 in choiceControl1.OptionSelections
-            //                 from s2 in choiceControl2.OptionSelections
-            //                 from s3 in choiceControl3.OptionSelections
-            //                 select new[] { s1, s2, s3 };
+            IDisposable subscription = null;
+            observerSelections.Subscribe(observer =>
+                                         {
+                                             if (subscription != null)
+                                                 subscription.Dispose();
 
-            //var selections = Observable
-            //    .ForkJoin(choiceControl1.OptionSelections, choiceControl2.OptionSelections, choiceControl3.OptionSelections);
+                                             choiceControl1.ClearAll();
+                                             choiceControl2.ClearAll();
+                                             choiceControl3.ClearAll();
+                                             statusText.Text = string.Empty;
 
-            var selections = choiceControl1.OptionSelections
-                .CombineLatest(choiceControl2.OptionSelections, (i, j) => new[] { i, j })
-                .CombineLatest(choiceControl3.OptionSelections, (array, k) => new[] { array[0], array[1], k });
+                                             subscription = observer.Subscribe(UpdateSelectedOptions);                                                                               
+                                         });
 
-            selections.Subscribe(values => statusText.Text = string.Format(formatString, values[0], values[1], values[2]));
-            
 
         }
 
+        private void UpdateSelectedOptions(int[] values)
+        {
+            statusText.Text = string.Format("Option {0}, Option {1}, Option {2}",
+                                            values[0], values[1], values[2]);
+        }
     }
 
 }
